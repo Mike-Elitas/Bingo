0d6662c9bd12c37375ce0e015c3824ecd301b0e2
Ivan Bosch <ivan.bosch.amengual@gmail.com>
2021-02-03 16:27:27 +0100

    initial

--- src/Bombo.java
+++ src/Bombo.java
@@ -8,39 +8,36 @@ import java.util.ArrayList;
 public class Bombo {
     boolean victoria = false;
     ArrayList<Integer> numeros = new ArrayList<>(100);
-
-    public int generar() {
-        int bola = ((int) (Math.random() * 100));
+    int bola;
+    public void generar() {
+        bola = ((int) (Math.random() * 100));
         System.out.println("\n");
         if (numeros.contains(bola)) bola = ((int) (Math.random() * 100));
         else numeros.add(bola);
         System.out.println("El número es: " + bola);
-        return bola;
     }
 
     public void game(Jugador jugador1, Jugador jugador2)throws Exception {
         while (!victoria) {
-            if (jugador1.cartulina1.size() != 0 && jugador1.cartulina2.size() != 0) {
+            if (jugador1.cartulina1.size() != 0 && jugador1.cartulina2.size() != 0 && jugador2.cartulina1.size() != 0 && jugador2.cartulina2.size() != 0) {
                 generar();
             } else if (jugador1.cartulina1.size() == 0 && jugador1.cartulina2.size() == 0){
                 System.out.println("Gana el jugador " + jugador1);
                 victoria = true;
-            } else if (jugador2.cartulina1.size() != 0 && jugador2.cartulina2.size() != 0) {
-                generar();
             } else if (jugador2.cartulina1.size() == 0 && jugador2.cartulina2.size() == 0){
                 System.out.println("Gana el jugador " + jugador1);
                 victoria = true;
             } else throw new Exception("Ha ocurrido algo inesperado.");
 
-            if (jugador1.cartulina1.contains(generar())) {
-                jugador1.cartulina1.remove(generar());
-            } else if (jugador1.cartulina2.contains(generar())) {
-                jugador1.cartulina2.remove(generar());
+            if (jugador1.cartulina1.contains(bola)) {
+                jugador1.cartulina1.remove(bola);
+            } else if (jugador1.cartulina2.contains(bola)) {
+                jugador1.cartulina2.remove(bola);
             }
-            if (jugador2.cartulina1.contains(generar())) {
-                jugador2.cartulina1.remove(generar());
-            } else if (jugador2.cartulina2.contains(generar())) {
-                jugador2.cartulina2.remove(generar());
+            if (jugador2.cartulina1.contains(bola)) {
+                jugador2.cartulina1.remove(bola);
+            } else if (jugador2.cartulina2.contains(bola)) {
+                jugador2.cartulina2.remove(bola);
             }
         }
 
